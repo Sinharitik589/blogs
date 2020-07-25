f6dd4696e1d06ef5976f0dca67cf5133d0f7a07d
Sinharitik589 <sinharitik18112835@gmail.com>
2020-07-25 11:31:08 +0530

    added files

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import Link from "next/link";
 import axios from "axios";
 
 export default function Index({ data }) {
-  console.log(process.env.BACKEND_URL, "jjjjjj");
   return (
     <>
       <p>Hello I am home page</p>
-      <Link href="/about" as={"/about"}>
+      <Link href="/about">
         <a>About</a>
       </Link>
       <h1>Series</h1>
@@ -16,7 +15,7 @@ export default function Index({ data }) {
           console.log(typeof data.title);
           return (
             <li key={data.id}>
-              <Link href="/show/[id]" as={`/show/${data.id}`}>
+              <Link href="/show/[id]" as={`blogs/show/${data.id}`}>
                 <a>{data.title}</a>
               </Link>
             </li>
