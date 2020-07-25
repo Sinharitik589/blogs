a92d8be2f79c1b1ec4a72c4cbf7867527c9e7e57
Sinharitik589 <sinharitik18112835@gmail.com>
2020-07-25 12:08:57 +0530

    changeddddd

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Index({ data }) {
   return (
     <>
       <p>Hello I am home page</p>
-      <Link href="/about">
+      <Link href="/blogs/about">
         <a>About</a>
       </Link>
       <h1>Series</h1>
